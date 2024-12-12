cb7abff34975e03d25e6290c5eb8bb3fe15a24c4
timmyjinks <tysonjjenkins.github@gmail.com>
2024-12-12 13:45:34 -0700

    Return html and Add better error handling

--- main.c
+++ main.c
@@ -1,10 +1,46 @@
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-int create_socket() {
+char *read_html(const char *filename) {
+    FILE *file;
+    file = fopen(filename, "rb");
+
+    if (!file) {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    char *buffer = (char *)malloc(file_size + 1);
+    if (!buffer) {
+        perror("Malloc failed");
+        fclose(file);
+        return NULL;
+    }
+
+    size_t read_size = fread(buffer, sizeof(char), file_size, file);
+
+    if (read_size != file_size) {
+        perror("Failed to open entire file");
+        free(buffer);
+        fclose(file);
+        return NULL;
+    }
+
+    buffer[read_size] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+int create_socket(const char *domain, const int port) {
     int server_fd;
     struct sockaddr_in server_addr;
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -15,8 +51,8 @@ int create_socket() {
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(domain);
 
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
@@ -28,7 +64,7 @@ int create_socket() {
         return -1;
     }
 
-    printf("Server listening on %d\n", 8080);
+    printf("Server listening on %d\n", port);
 
     return server_fd;
 }
@@ -44,21 +80,47 @@ void handle_requests(int server_fd) {
         char buffer[1024];
         recv(client_fd, buffer, sizeof(buffer), 0);
 
-        char *response =
-            "HTTP/1.1 200 OK\r\nContent-Type:"
-            "text/plain\r\nContent-Length: 5\r\n\r\nhello";
+        char *headers =
+            "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %ld; "
+            "charset=UTF-8\r\n\r\n%s";
+        char *html = read_html("index.html");
+
+        if (!html) {
+            const char *response = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+            send(client_fd, response, strlen(response), 0);
+            close(client_fd);
+            continue;
+        }
+
+        char response[strlen(headers) + strlen(html)];
+        snprintf(response, sizeof(response), headers, strlen(html), html);
+
         send(client_fd, response, strlen(response), 0);
+
         close(client_fd);
+        free(html);
     }
 }
 
-void start() {
-    int socket = create_socket();
+int main(int argc, char *argv[]) {
+    if (argc > 3) {
+        printf("To many arguments\n");
+        return 0;
+    } else if (argc < 3) {
+        printf("What the fuck are you doin\n");
+        return 0;
+    }
+
+    const char *domain = argv[1];
+    const int port = atoi(argv[2]);
+
+    int socket = create_socket(domain, port);
+    if (socket < 0) {
+        perror("Server creation failed");
+        exit(1);
+    }
+
     handle_requests(socket);
     close(socket);
-}
-
-int main() {
-    start();
     return 0;
 }
